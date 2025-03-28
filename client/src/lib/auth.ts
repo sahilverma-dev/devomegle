ab1a8b1e32d269b2d44b8ae6429103fbeae203c3
Sahil Verma <domainsahil007@gmail.com>
2025-03-28 12:59:09 +0530

    feat: implement authentication middleware and email validation, update home page structure

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -1,10 +1,25 @@
 import { betterAuth } from "better-auth";
+import { createAuthMiddleware, APIError } from "better-auth/api";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 
 import * as schema from "@/db/schema/user-schema";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  hooks: {
+    before: createAuthMiddleware(async (ctx) => {
+      if (ctx.path !== "/login") {
+        return;
+      }
+      if (!ctx.body?.email.endsWith("@example.com")) {
+        throw new APIError("BAD_REQUEST", {
+          message: "Email must end with @example.com",
+        });
+      }
+    }),
+  },
+
   database: drizzleAdapter(db, {
     provider: "sqlite",
     schema,
@@ -26,4 +41,8 @@ export const auth = betterAuth({
       // }),
     },
   },
+
+  plugins: [nextCookies()],
 });
+
+export const { getSession } = auth.api;
