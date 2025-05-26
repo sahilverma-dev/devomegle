1f4978cd8df6a5790224f874101ac8973bd1b59e
Sahil Verma <domainsahil007@gmail.com>
2025-05-26 09:11:39 +0530

    refactor: remove Redis integration and related user handling logic; update TypeScript configuration for output directory

--- server/src/redis.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "redis";
-
-export const redis = createClient({
-  password: process.env.REDIS_PWD,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: parseInt(process.env.REDIS_PORT!),
-  },
-});
